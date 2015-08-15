677683bae602849a7ffaab347795d99fc0880310
Saif Syed <saif.syed@orionhealth.com>
2015-08-16 01:18:20 +1200

    master 2

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		
 		System.out.println("master1");
+		System.out.println("master2");
 		System.out.println("test4");
 		
 		System.out.println("test11");
