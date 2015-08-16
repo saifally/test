e58cd1fe64993b826bfe22c48d30ebfc69ea4102
Saif Syed <saif.syed@orionhealth.com>
2015-08-16 22:40:46 +1200

    3-1 3-2 3-3

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,6 +5,10 @@ public class test {
 	public static void main(String[] args) {
 		
 		System.out.println("master3");
+		System.out.println("master3-1");
+		System.out.println("master3-2");
+		System.out.println("master3-2");
+		System.out.println("master3-3");
 		System.out.println("master4");
 		System.out.println("test4");
 		
